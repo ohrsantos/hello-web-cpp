51f593b6e4b04239582984155381dd7509b7051f
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-19 13:19:51 -0300

    .

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
    "service": {
        "list": [
            {"ip": "127.0.0.1", "api": "http", "port": 8080},
-           {"ip": "10.42.59.43", "api": "http", "port": 8080},
+           {"ip": "CONTAINER_IP", "api": "http", "port": 8080},
      ]
    },
     "http" : {
