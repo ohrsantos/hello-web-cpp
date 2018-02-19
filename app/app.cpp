c75b14e6a91d56c304b7c4f33da6b543b48d3fde
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-19 15:55:41 -0300

    .

--- app/app.cpp
+++ app/app.cpp
@@ -18,7 +18,7 @@ void my_hello_world::main(std::string /*url*/)
     response().out()<<
         "<html>\n"
    "<head>"
-        "<title>Provérbios</title><meta charset=\"utf-8\">"
+        "<title>CppCMS App Lab</title><meta charset=\"utf-8\">"
         "<meta charset=\"utf-8\">"
         "<!-- Latest compiled and minified CSS --> "
         "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">"
