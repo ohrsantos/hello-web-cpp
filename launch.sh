fabec86f89f14bda6c065fb98a71e269513982d3
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-19 13:31:02 -0300

    .

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 cd app
 
 IP=$(ip addr  | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1 | awk -F'/' '{print $1}')
-sed -i -e "s/x/$IP/g" config.js
+sed -i -e "s/CONTAINER_IP/$IP/g" config.js
 
 date
 ./app -c config.js 
