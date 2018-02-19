6145c093e19654ff30447d252b2e23810dab7cf0
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-19 13:59:19 -0300

    Initial commit

--- containers/build-base-cppcms.sh
+++ containers/build-base-cppcms.sh
@@ -1,5 +1,5 @@
-docker build -f containers/Dockerfile.base-cppcms  -t ohrsan/base-cppcms:latest .
+docker build -f containers/Dockerfile.app-cppcms  -t ohrsan/app-cppcms:latest .
 
 docker login -u=ohrsan -p=bomdia01
 
-docker push ohrsan/base-cppcms:latest
+docker push ohrsan/app-cppcms:latest
